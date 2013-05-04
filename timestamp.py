9d1ac19cc4330a7195c6b9c29cf1045ebb5065b9
Evan Caldwell <evan.caldwell@gmail.com>
2013-05-03 22:12:06 -0400

    changed timestamp from class to module

--- timestamp.py
+++ timestamp.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 import datetime
-
-class Timestamp:
    
-    def pretty(self):
-        '''Return the current time in the format 2012-06-14 2:24:56pm.'''
-        # Create a timestamp in the format yearmonthdayhoursminutesseconds'''
-        theTime = datetime.datetime.now()
-        timestamp = theTime.strftime("%Y-%m-%d %I:%M:%S%p")
-        return timestamp
-    
-    def raw(self):
-        '''Return the current time in the format 20120614142456.'''
-        # Create a timestamp in the format yearmonthdayhoursminutesseconds'''
-        theTime = datetime.datetime.now()
-        timestamp = theTime.strftime("%Y%m%d%H%M%S")
-        return timestamp
+def pretty():
+    '''Return the current time in the format 2012-06-14 2:24:56pm.'''
+    # Create a timestamp in the format yearmonthdayhoursminutesseconds'''
+    theTime = datetime.datetime.now()
+    timestamp = theTime.strftime("%Y-%m-%d %I:%M:%S%p")
+    return timestamp
+
+def raw():
+    '''Return the current time in the format 20120614142456.'''
+    # Create a timestamp in the format yearmonthdayhoursminutesseconds'''
+    theTime = datetime.datetime.now()
+    timestamp = theTime.strftime("%Y%m%d%H%M%S")
+    return timestamp
