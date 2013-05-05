d3fa621d358aea7ea7f9fdf0f5bb7d147afc42b3
Evan Caldwell <evan.caldwell@gmail.com>
2013-05-05 12:18:05 -0400

    add unix timestamp option

--- timestamp.py
+++ timestamp.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 import datetime
+import time
    
 def pretty():
     '''Return the current time in the format 2012-06-14 2:24:56pm.'''
-    # Create a timestamp in the format yearmonthdayhoursminutesseconds'''
     theTime = datetime.datetime.now()
     timestamp = theTime.strftime("%Y-%m-%d %I:%M:%S%p")
     return timestamp
 
 def raw():
     '''Return the current time in the format 20120614142456.'''
-    # Create a timestamp in the format yearmonthdayhoursminutesseconds'''
     theTime = datetime.datetime.now()
     timestamp = theTime.strftime("%Y%m%d%H%M%S")
     return timestamp
+
+def unix():
+    '''Return the current time in the format 1367770445.'''
+    theTime time.time()
+    return int(theTime)
